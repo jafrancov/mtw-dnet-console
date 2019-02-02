5569a0cec3d10d79f42dba3150fe9c40f253df03
Alejandro Franco <jafrancov@MBPr-jafrancov.local>
2019-02-02 09:01:11 -0600

    Práctica 1 .Net Console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mtw_dnet_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
